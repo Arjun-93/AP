0f92f5f9cf4a8d847c259cf7b04b079228b02c52
Arjun Mehra <you@example.com>
2022-12-12 19:51:28 +0530

    AP_commited

---
+++ AP_Endsem_Preparation/Classes_objects.java
@@ -0,0 +1,17 @@
+package AP_Endsem_Preparation;
+
+public class Classes_objects {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+class Main{
+
+    private int a;
+    private int b;
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
