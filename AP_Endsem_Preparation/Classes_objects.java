6c6cb1f278509cb7799c8a8c2ae2b2daa89aa214
Arjun Mehra <you@example.com>
2022-12-13 11:49:33 +0530

    class and objects in oop

--- AP_Endsem_Preparation/Classes_objects.java
+++ AP_Endsem_Preparation/Classes_objects.java
@@ -2,10 +2,11 @@ package AP_Endsem_Preparation;
 
 public class Classes_objects {
     public static void main(String[] args) {
-        
+        Main m1 = new Main(1,2);
+        m1.print();
+
     }
 }
-
 
 class Main{
 
@@ -13,5 +14,27 @@ class Main{
     private int b;
     public static void main(String[] args) {
         System.out.println("Hello");
+    }
+    public Main(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public int getA(){
+        return a;
+    }
+    public int getB(){
+        return b;
+    }
+    public void setA(int a){
+        this.a = a;
+    }
+    public void setB(int b){
+        this.b = b;
+    }
+    public void print(){
+        System.out.println("a = " + a + " b = " + b);
+    }
+    public String toString(){
+        return "a = " + a + " b = " + b;
     }
 }
